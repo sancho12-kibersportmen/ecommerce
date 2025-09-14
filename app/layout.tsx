8141d9943b2aabdba39c52947c79dce9f888d3ae
sancho12-kibersportmen <kuntunak@gmail.com>
2025-09-14 23:10:54 +0300

    acu perger

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,16 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
-import "./globals.css";
+
+import {  Inter } from "next/font/google";
+
 import {ClerkProvider} from "@clerk/nextjs";
-// import { ModalProvider } from "@/providers/modal-provider";
+ import { ModalProvider } from "@/providers/modal-provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import './globals.css';
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Admin Dashboard",
   description: "Admin Dashboard",
 };
@@ -32,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className}>
+        <ModalProvider />
           {children}
         </body>
       </html>
